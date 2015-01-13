069ada69c214322a4bea8060a8279c0a9ffb53bb
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-13 13:41:13 -0500

    minor change

--- palindrome.py
+++ palindrome.py
@@ -2,18 +2,18 @@ def preprocess(sentence):
     sentencelist=[]
     for letters in sentence:
         if letters.isalpha():
-            sentencelist.append(letters)
+            sentencelist.append(letters.lower())
     return sentencelist
 
 print("I will check if the sentence you enter is a palindrome")
 sentence = input("Enter a Sentence ")
-sentence=preprocess(sentence.lower())
+sentence=preprocess(sentence)
 newsentence=[]
 
 for letters in sentence[::-1]:
     newsentence.append(letters)
 
-if list(sentence) == newsentence:
+if sentence == newsentence:
     print("YES!!!!!!  It is a palindrome")
 else:
     print("NO!!!!  It is not a palindrome")
