c3bb8a390718b88195cb551d96973339422a4a91
Dean Fitzgerald <deanfitzgerald@HP-Photosmart.local>
2015-01-13 16:41:47 -0500

    the real one?

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,16 @@
 #we need to compare the first index and the last, remove those two and
 #repeat.
+
 import re
 text = input("Feed me text: ")
+original_text = text
 
 def pal2(text):
     lower = (text).lower()
     stripped = re.sub(r'[^A-Za-z]',"", lower)
 
-    if len(stripped) == 1:
-        return "{} is a palindrome".format(text)
+    if len(stripped) <= 1:
+        return "{} is a palindrome.".format(original_text)
 
     elif stripped[0] == stripped[-1]:
         text1 = stripped[1:-1]
@@ -16,9 +18,8 @@ def pal2(text):
 
 
     elif text[0] != text[-1]:
-            print ("This is not a palindrome.")
+        return ("This is not a palindrome.")
 
-    else:
-        return "Oops"
 
-pal2(text)
+
+print(pal2(text))
