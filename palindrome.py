dcca58d246d376b6b72f188bca34a9a4d8cdac51
Bret Runestad <bret.runestad@gmail.com>
2015-01-13 15:54:59 -0500

    Added iterative function

--- palindrome.py
+++ palindrome.py
@@ -10,9 +10,9 @@ string_clean = re.sub(r'[^A-Za-z]', '', string_low)
 print (string_clean)
 
 
-
 def trim(string_clean):
-    """A function which compares and trims the beginning and ends of a clean string to determine whether it is a palindrome"""
+    """A recursive function which compares and trims the beginning and ends
+    of a clean string to determine whether it is a palindrome"""
 
     if len(string_clean) > 1:
         if string_clean[0] == string_clean[-1]:
@@ -21,6 +21,17 @@ def trim(string_clean):
             return False
     else:
         return True
+
+def iter_trim(string_clean):
+    """An iterative function which compares and trims the beginning and ends
+     of a clean string to determine whether it is a palindrome"""
+    str_length = len(string_clean)
+    while str_length > 1:
+        if string_clean[0] == string_clean[-1]:
+            string_clean = string_clean[1: -1]
+            str_length = str_length -2
+        else: return False
+    return True
 
 if trim(string_clean):
     print("{} is a palindrome!".format(guess))
