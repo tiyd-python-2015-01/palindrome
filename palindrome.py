de486f58cf40e06639f40da75ad91a497b467c31
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-01-13 16:45:02 -0500

    Added messages to each script, and edited for pep8.

--- palindrome.py
+++ palindrome.py
@@ -2,11 +2,13 @@ import re
 
 
 def palindrome(words):
-    """ Will return if a word is or is not a palindrome """
-    print("The computer is comparing the original string with the reverse of the string.")
+    """Will return if a word is a palindrome."""
+    print("Comparing the original string with the reverse of the string...")
     if words[::-1] == words:
+        print("The string reversed was the same as the normal string!")
         print("is a palindrome")
     else:
+        print("The string reversed was not the same as the normal string.")
         print("is not a palindrome")
 
 words = input("Please enter a word: ")
--- palindrome_iterative.py
+++ palindrome_iterative.py
@@ -1,21 +1,22 @@
 import re
 
-
 def palindrome(words):
-    """ Will return if a word is or is not a palindrome """
+    """Will return if a word is a palindrome, through an iterative method."""
+    words = re.sub(r'[^A-Za-z]', '', words)
     counter = -1
     check_word = []
+    print("The function will create a new list of characters")
+    print("by appending the last character of the string, and moving backward.")
     while not len(check_word) == len(words):
+        print('{} was appended'.format(words[counter]))
         check_word.append(words[counter])
-        print(words, check_word)
         counter -= 1
     check_word = ''.join(check_word)
-    print(check_word)
+    print('The reverse of the given word is {}'.format(check_word))
     if words == check_word:
         print('is a palindrome')
     else:
         print('is not a palindrome')
 
 words = input('Please enter a word: ')
-words = re.sub(r'[^A-Za-z]', '', words)
-palindrome(words.lower)
+palindrome(words.lower())
